9a589a6160ea7c15748ecb896489413ac7c19283
ChilenoSika <jaum97@yahoo.com.br>
2018-11-09 02:23:52 -0200

    Frontend done

--- frontend/src/pages/Timeline.js
+++ frontend/src/pages/Timeline.js
@@ -2,12 +2,35 @@ import React, { Component } from 'react';
 import api from '../services/api';
 import './Timeline.css';
 import twitterLogo from '../twitter.svg';
+import Tweet from '../components/Tweet';
+import socket from 'socket.io-client';
+
 
 export default class Timeline extends Component {
 
     state = {
+        tweets: [],
         newTweet: '',
     };
+
+    async componentDidMount() {
+        this.subscribeToEvents();
+        const response = await api.get('tweets');
+        this.setState({ tweets: response.data });
+    };
+
+    subscribeToEvents = () => {
+        const io = socket('http://localhost:3000');
+        io.on('tweet', data=>{
+            this.setState({ tweets:[data, ...this.state.tweets] });
+        });
+        io.on('like', data=>{
+            this.setState({ tweets: this.state.tweets.map(tweet =>
+            tweet._id === data._id ? data : tweet)
+            })
+        });
+    };
+
     handleInputChange = e => {
         this.setState({ newTweet: e.target.value });
     };
@@ -34,7 +57,11 @@ export default class Timeline extends Component {
                     />
 
                 </form>
-
+                <ul className="tweet-list">
+                {this.state.tweets.map(tweet => (
+                    <Tweet key={tweet._id} tweet={tweet}/>
+                ))}
+                </ul>
             </div>
         );
     }
