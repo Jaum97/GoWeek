7c9855e432092017b9cbdbb7462479e4318090c7
ChilenoSika <jaum97@yahoo.com.br>
2018-11-09 00:41:29 -0200

    Storing tweets

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,34 @@
 import React, { Component } from 'react';
 import './Login.css';
+import twitterLogo from '../twitter.svg';
 export default class Login extends Component {
+    state = {
+        username: '',
+    };
+
+    handleInputChange = (e) => {
+        this.setState({ username: e.target.value });
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        const { username } = this.state;
+        if (!username.length) return;
+
+        localStorage.setItem('@GoTwitter:username', username);
+        this.props.history.push('/timeline');
+
+    }
+
     render() {
-        return <h1>Login</h1>;
+        return (
+            <div className="login-wrapper">
+                <img src={twitterLogo} alt="GoTwitter" />
+                <form onSubmit={this.handleSubmit}>
+                    <input value={this.state.username} placeholder="User Name" onChange={this.handleInputChange}></input>
+                    <button type="submit">Enter</button>
+                </form>
+            </div>
+        );
     }
 }
--- frontend/src/pages/Timeline.js
+++ frontend/src/pages/Timeline.js
@@ -1,7 +1,41 @@
 import React, { Component } from 'react';
+import api from '../services/api';
+import './Timeline.css';
+import twitterLogo from '../twitter.svg';
 
 export default class Timeline extends Component {
+
+    state = {
+        newTweet: '',
+    };
+    handleInputChange = e => {
+        this.setState({ newTweet: e.target.value });
+    };
+    handleNewTweet = async e => {
+        if (e.keyCode !== 13) return;
+
+        const content = this.state.newTweet;
+        const author = localStorage.getItem('@GoTwitter:username');
+
+        await api.post('tweets', { content, author });
+        this.setState({ newTweet: '' });
+
+    };
     render() {
-        return <h1>Timeline</h1>
+        return (
+            <div className="timeline-wrapper">
+                <img height={24} src={twitterLogo} alt="GoTwitter" />
+                <form>
+                    <textarea
+                        value={this.state.newTweet}
+                        onChange={this.handleInputChange}
+                        onKeyDown={this.handleNewTweet}
+                        placeholder=" Whats new ?"
+                    />
+
+                </form>
+
+            </div>
+        );
     }
 }
